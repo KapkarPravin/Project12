a9da6c94c64bff8309d7d30b535886c8003c3323
KapkarPravin <pravin.kapkar26@gmail.com>
2023-05-17 23:45:59 +0530

    This is class2

---
+++ Class2.java
@@ -0,0 +1,53 @@
+package package2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int number=149;
+		int number=11;
+		if (number==1) {
+			System.out.println("January");
+		}
+		else if (number==2) {
+			System.out.println("February");
+		}
+		else if (number==3) {
+			System.out.println("March");
+		}
+		else if (number==4) {
+			System.out.println("April");
+		}
+		else if (number==5) {
+			System.out.println("May");
+		}
+		else if (number==6) {
+			System.out.println("June");
+		}
+		else if (number==7) {
+			System.out.println("Jully");
+		}
+		else if (number==8) {
+			System.out.println("August");
+		}
+		else if (number==9) {
+			System.out.println("September");
+		}
+		else if (number==10) {
+			System.out.println("Octomber");
+		}
+		else if (number==11) {
+			System.out.println("November");
+		}
+		else if (number==12) {
+			System.out.println("December");
+		}
+		else {
+			System.out.println("invalid month");
+		}
+
+
+}
+
+	}
